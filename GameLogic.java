f69fd17feef16a6f40185caf031d68f27416d40e
otteranne <jannie042@gmail.com>
2026-06-03 13:56:31 +0800

    add core quiz files

---
+++ GameLogic.java
@@ -0,0 +1,14 @@
+/**
+ * Interface  : GameLogic
+ * Creator    : Member 3
+ * Tester     : Member 3
+ * Description: Defines the core game logic contract for the survival quiz simulation.
+ */
+public interface GameLogic {
+
+    // Starts a countdown timer with given seconds
+    void startSurvivalTimer(int seconds);
+
+    // Awards or skips points based on whether answer was correct
+    void calculatePoints(boolean isCorrect);
+}
